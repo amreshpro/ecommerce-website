995ad3cb1c2b77c1143a20c7a93a00f8c4ec2ab1
Amresh Maurya <amresh.terminal@gmail.com>
2023-07-30 22:37:00 +0530

    improved login

--- src/app/components/Login.jsx
+++ src/app/components/Login.jsx
@@ -67,9 +67,10 @@ dispatch(userLoginDetailsForStore(user))
                       name="name"
                       type="text"
                       autoComplete="name"
+                      placeholder="Enter your username"
                       required
                       onChange={(e)=>{setUser({...user,name:e.target.value})}}
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      className="block w-full rounded-md border-0 py-1.5 px-2 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
                   </div>
                 </div>
@@ -86,7 +87,7 @@ dispatch(userLoginDetailsForStore(user))
                       autoComplete="email"
                       required
                       onChange={(e)=>{setUser({...user,email:e.target.value})}}
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
                   </div>
                 </div>
@@ -110,7 +111,7 @@ dispatch(userLoginDetailsForStore(user))
                       autoComplete="current-password"
                       required
                       onChange={(e)=>{setUser({...user,password:e.target.value})}}
-                      className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                      className="block w-full rounded-md border-0 px-2 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                     />
                   </div>
                 </div>
